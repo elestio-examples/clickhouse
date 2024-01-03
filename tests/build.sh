7c269d02dc5cb0228f68f212308004f8e554793a
Prem chavhan <pchavhan@logiquad.com>
2024-01-03 13:28:31 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rm -f docker/server/Dockerfile
+rm -f docker/server/README.md
 cp -rf docker/server/* ./
 mv Dockerfile.ubuntu Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/clickhouse-server:latest | docker load
